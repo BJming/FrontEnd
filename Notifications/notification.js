506b4e634dd54f63c1f6e2f017e44e89df5f1e96
BJming <bjm18767327940>
2017-03-07 22:20:26 +0800

    【修复】notification.js脚本中的错误

--- Notifications/notification.js
+++ Notifications/notification.js
@@ -2,26 +2,30 @@
  * Created by Administrator on 2017/3/7.
  */
 function notice(option) {
-    if(window.Notification){
+    if (window.Notification) {
         //支持
-        if(window.Notification && Notification.permission !== "denied"){
+        if (window.Notification && Notification.permission !== "denied") {
             Notification.requestPermission(function (status) {
                 var notification = new Notification('收到新邮件', option);//已经执行了
-                notification.onshow = function() {
+                notification.onshow = function () {
                     console.log('Notification shown !!!');
                     setTimeout(notification.close.bind(notification), 5000);//设置5秒延时关闭，提高用户体验
                 };
                 notification.onclick = function () {
-                    if(confirm("是否前去查看消息")){
+                    if (confirm("是否前去查看消息")) {
                         notification.close();
-                        window.location='http://localhost:8080/order/org_order_mng.html';
+                        window.location = 'http://localhost:8080/order/org_order_mng.html';
                     }
                 }
             })
+        } else {
+            console.log("code:444 no permision")
+            alert("Hi!");
         }
 
-    }else {
+    } else {
         //不支持
+        console.log("code:555 no notification")
         alert("Hi!");
     }
 }
